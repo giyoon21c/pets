7be09fbe64d05366b7b65b5ddd881ca2acac5f0b
Your Name <giyoon21c@gmail.com>
2017-01-22 08:09:50 -0800

    starting with contract class done

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -6,15 +6,29 @@ import android.provider.BaseColumns;
 public final class PetContract {
     public static final String CONTENT_AUTHORITY = "com.example.android.pets.data";
 
+    /**
+     *
+     */
+    private PetContract() {}
+
+    /**
+     *
+     */
     public static class PetEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "pets";
 
-        public static final String COLUMN_ID = "_id";
-        public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_BREED = "breed";
-        public static final String COLUMN_GENDER = "gender";
-        public static final String COLUMN_WEIGHT = "weight";
-        
+        public static final String _ID = "_id";
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * Possible values for gender constants
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
     }
 }
