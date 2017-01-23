f34fbb9f2f8d82edc745746d02fa939a510f319f
Your Name <giyoon21c@gmail.com>
2017-01-23 07:10:28 -0800

    PetDbHelper class added

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -30,6 +30,6 @@ public final class PetContract {
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
-        
+
     }
 }
---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,51 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.pets.data.PetContract.PetEntry;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    // Database Info
+    private static final String DATABASE_NAME = PetEntry.TABLE_NAME;
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     *
+     * @param context
+     */
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     *
+     * @param db
+     * CREATE TABLE pets(_id INTEGER PRIMARY KEY AUTOINCREMENT,
+     *                   name TEXT NOT NULL,
+     *                   breed TEXT,
+     *                   gender INTEGER NOT NULL,
+     *                   weight INTEGER NOT NULL DEFAULT 0
+     *                   );
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE " + DATABASE_NAME + " ( " +
+                PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL," +
+                PetEntry.COLUMN_PET_BREED + " TEXT," +
+                PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, " +
+                PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0" +
+                ")";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
+        onCreate(db);
+    }
+
+}
