35d584aeefdb000ce185c799bd37917e81bb3c6c
Your Name <giyoon21c@gmail.com>
2017-02-22 08:00:38 -0800

    content provider

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -29,7 +29,7 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
-        // Setup FAB to open EditorActivity
+        // Setup FAB - Floating Action Button - to open EditorActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,10 +1,14 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 
 public final class PetContract {
-    public static final String CONTENT_AUTHORITY = "com.example.android.pets.data";
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
 
     /**
      *
@@ -17,6 +21,7 @@ public final class PetContract {
     public static class PetEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "pets";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         public static final String _ID = "_id";
         public static final String COLUMN_PET_NAME = "name";
